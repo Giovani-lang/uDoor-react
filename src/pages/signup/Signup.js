3b797b208eaee38b78a7492e5abbc5aef5dbe0e9
Giovani-lang <tchicayavytika@gmail.com>
2023-10-06 19:17:23 +0100

    update input

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -63,116 +63,116 @@ const Signup = () => {
                     <div className="form-login">
                         <h1>Create your <br /> account </h1>
                         <Form onSubmit={handleSubmit}>
-                          <div style={{display:'flex'}}>
-                          <Form.Item
-                                name={"firstname"}
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please, enter your firstname',
+                            <div style={{ display: 'flex' }}>
+                                <Form.Item
+                                    name={"firstname"}
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please, enter your firstname',
 
-                                    },
-                                    {
-                                        type: "text",
-                                        message: 'firstname is not valid',
-                                        warningOnly: true,
+                                        },
+                                        {
+                                            type: "text",
+                                            message: 'firstname is not valid',
+                                            warningOnly: true,
 
-                                    },
-                                ]}>
-                                <Input onChange={handleInput} placeholder='firstname' style={{width:'250px', marginRight:'10px'}} />
-                            </Form.Item>
-                            <Form.Item
-                                name={"lastname"}
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please, enter your lastname',
+                                        },
+                                    ]}>
+                                    <Input onChange={handleInput} placeholder='firstname' style={{ width: '250px', marginRight: '10px' }} />
+                                </Form.Item>
+                                <Form.Item
+                                    name={"lastname"}
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please, enter your lastname',
 
-                                    },
-                                    {
-                                        type: "text",
-                                        message: 'lastname is not valid',
-                                        warningOnly: true,
+                                        },
+                                        {
+                                            type: "text",
+                                            message: 'lastname is not valid',
+                                            warningOnly: true,
 
-                                    },
-                                ]}
-                            >
-                                <Input onChange={handleInput} placeholder='lastname' style={{width:'250px', marginRight:'10px'}}/>
-                            </Form.Item>
-                          </div>
-                       <div style={{display:'flex'}}>
-                       <Form.Item
-                                name={"email"}
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please, enter your email',
+                                        },
+                                    ]}
+                                >
+                                    <Input onChange={handleInput} placeholder='lastname' style={{ width: '250px', marginRight: '10px' }} />
+                                </Form.Item>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Form.Item
+                                    name={"email"}
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please, enter your email',
 
-                                    },
-                                    {
-                                        type: "email",
-                                        message: 'email is not valid',
-                                        warningOnly: true,
+                                        },
+                                        {
+                                            type: "email",
+                                            message: 'email is not valid',
+                                            warningOnly: true,
 
-                                    },
-                                ]}
-                            >
-                                <Input onChange={handleInput} placeholder='email'style={{width:'250px', marginRight:'10px'}} />
+                                        },
+                                    ]}
+                                >
+                                    <Input onChange={handleInput} placeholder='email' style={{ width: '250px', marginRight: '10px' }} />
 
-                            </Form.Item>
-                            <Form.Item
-                                name={"password"}
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please, enter your password',
+                                </Form.Item>
+                                <Form.Item
+                                    name={"password"}
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please, enter your password',
 
-                                    },
-                                    {
-                                        type: "password",
-                                        message: 'password is not valid',
-                                        warningOnly: true,
+                                        },
+                                        {
+                                            type: "password",
+                                            message: 'password is not valid',
+                                            warningOnly: true,
 
-                                    },
-                                ]}>
-                                <Input.Password onChange={handleInput} placeholder='password'style={{width:'250px', marginRight:'10px'}} />
-                            </Form.Item>
-                       </div>
-                      <div style={{display:'flex'}}>
-                      <Form.Item >
-                                <Space.Compact>
-                                    <Form.Item
-                                        name={'profil'}
-                                        noStyle
-                                        rules={[{ required: true, message: 'Profil is required' }]}
-                                    >
-                                        <Select placeholder="Select profil"style={{width:'250px', marginRight:'10px'}}>
-                                            <Option value="User">User</Option>
-                                            <Option value="Admin">Admin</Option>
-                                        </Select>
-                                    </Form.Item>
-                                </Space.Compact>
-                            </Form.Item>
-                            <Form.Item
-                                name={"phone"}
-                                rules={[
-                                    {
-                                        required: true,
-                                        message: 'Please, enter your phone number',
+                                        },
+                                    ]}>
+                                    <Input.Password onChange={handleInput} placeholder='password' style={{ width: '250px', marginRight: '10px' }} />
+                                </Form.Item>
+                            </div>
+                            <div style={{ display: 'flex' }}>
+                                <Form.Item >
+                                    <Space.Compact>
+                                        <Form.Item
+                                            name={'profil'}
+                                            noStyle
+                                            rules={[{ required: true, message: 'Profil is required' }]}
+                                        >
+                                            <Select placeholder="profil" style={{ width: '250px', marginRight: '10px' }}>
+                                                <Option value="User">User</Option>
+                                                <Option value="Admin">Admin</Option>
+                                            </Select>
+                                        </Form.Item>
+                                    </Space.Compact>
+                                </Form.Item>
+                                <Form.Item
+                                    name={"phone"}
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Please, enter your phone number',
 
-                                    },
-                                    {
-                                        type: "text",
-                                        message: 'phone number is not valid',
-                                        warningOnly: true,
+                                        },
+                                        {
+                                            type: "text",
+                                            message: 'phone number is not valid',
+                                            warningOnly: true,
 
-                                    },
-                                ]}>
-                                <Input onChange={handleInput} placeholder='phone'style={{width:'250px', marginRight:'10px'}} />
-                            </Form.Item>
-                      </div>
+                                        },
+                                    ]}>
+                                    <Input onChange={handleInput} placeholder='phone' style={{ width: '250px', marginRight: '10px' }} />
+                                </Form.Item>
+                            </div>
                             <Form.Item>
-                            <Button htmlType='submit' type="primary" style={{width:'150px'}} onClick={handleSubmit}>Register</Button>
+                                <Button htmlType='submit' type="primary" style={{ width: '150px' }} onClick={handleSubmit}>Register</Button>
                             </Form.Item>
                             <div className="navigation"><Signin /></div>
                         </Form>
