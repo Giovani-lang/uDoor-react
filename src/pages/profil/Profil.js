5aeb3972ca1de201b3e23401ce0eef06e696c08b
tchicayavytika@gmail.com <tchicayavytika@gmail.com>
2023-11-14 10:26:25 +0100

    Ok

--- src/pages/profil/Profil.js
+++ src/pages/profil/Profil.js
@@ -1,14 +1,23 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Profil.css'
+import { Card } from 'antd';
+import './Profil.css';
+import { Tabs } from 'antd';
+import { message, Form, Input, Button, Select, Space } from 'antd';
+import { MailOutlined, LockOutlined, UserOutlined, PhoneOutlined, MonitorOutlined } from '@ant-design/icons';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import { imageDB } from '../../components/user/firebase-config'
+import { v4 } from 'uuid'
 
 
+
+const { Option } = Select;
 
 const Profil = () => {
-    const history = useNavigate();
 
-    let email;
+
+    const history = useNavigate();
 
     const [user, setUser] = useState({
         firstname: "",
@@ -21,40 +30,247 @@ const Profil = () => {
         image_url: ""
     });
 
+    let email;
     useEffect(() => {
         email = sessionStorage.getItem('email');
         if (email === '' || email === null) {
             history('/Signin')
         }
-
-        getProfil();
     }, []);
 
-    const getProfil = () => {
-        axios.get('https://test-back.authentify.upowa.org/api/user/detail?email=' + email)
-            .then((resp) => {
-                console.log(resp.data)
-                setUser(resp.data)
-                console.log(user)
-            }).catch((err) => {
-                console.log(err)
-            })
+    const [image, setImage] = useState("")
+
+    const handleImageSelected = (event) => {
+        const img = event.target.files[0];
+        setImage(img)
     }
 
-    return (
-        <div>
-            <div className='convert-image'>
-                {/* <img src={user.image_url} /> */}
-            </div>
-            <div className='content'>
-                Hello, world !
+    const handleImageUpload = async () => {
+        if (image) {
+            const storageRef = ref(imageDB, `udoor/images/${v4()}`);
+            try {
+                const snapshot = await uploadBytes(storageRef, image);
 
-                {/* <p>{user.firstname}</p>
-                <p>{user.lastname}</p>
-                <p>{user.email}</p>
-                <p>{user.phone}</p>
-                <p>{user.profil}</p>
-                <p>{user.statut}</p> */}
+                const downloadURL = await getDownloadURL(snapshot.ref);
+                return downloadURL
+            } catch (error) {
+                console.error('Erreur lors du téléchargement de l\'image :', error);
+                throw error;
+            }
+        }
+    }
+
+    const onUpdate = async (values) => {
+        const imageUrl = await handleImageUpload();
+        values.image_url = imageUrl
+        console.log(values)
+        axios.put('https://test-back.authentify.upowa.org/api/user/update/' + email, values)
+            .then(resp => {
+                setUser(resp)
+                if (resp.status === 201) {
+                    message.success('Successfully')
+                    history('/Signin');
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        axios.interceptors.response.use(response => {
+            return response;
+        }, err => {
+            if (err.response.status === 409) {
+                console.log('This user already exist !')
+                message.warning('This user already exist, try again !')
+            } else if (err.response.status === 500) {
+                message.warning('A problem occurred while saving information !')
+            }
+        })
+    };
+
+
+
+    return (
+        <div className='profil'>
+
+            <Card
+                style={{
+
+                    backgroundImage: 'linear-gradient(#0078d4,white,white)',
+                    display: 'flex',
+                    width: 300,
+                    height: 350,
+                    justifyContent: 'center',
+                }}
+            >
+                <div
+                    className='.convert-image '
+                >
+                    <div style={{ textAlign: 'center' }}>
+                        <h2 >{user.firstname} {user.lastname}</h2>
+                    </div>
+                    <div className='image'>
+                        <img
+                            className='img'
+                            src={user.image_url || 'https://upload.wikimedia.org/wikipedia/commons/b/b5/Windows_10_Default_Profile_Picture.svg'}
+                        />
+                    </div>
+                </div>
+            </Card>
+
+            <div className='content'>
+                <Card
+                    style={{
+                        width: 600,
+                    }}
+                >
+                    <div style={{ display: 'flex' }}>
+                        <Form.Item
+                            name={"firstname"}
+                            initialValue={user.firstname}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please, enter your firstname',
+
+                                },
+                                {
+                                    type: "text",
+                                    message: 'firstname is not valid',
+                                    warningOnly: true,
+
+                                },
+                            ]}>
+                            <Input placeholder='firstname' style={{ width: '230px', marginRight: '10px' }}
+                                prefix={<UserOutlined />} />
+                        </Form.Item>
+                        <Form.Item
+                            name={"lastname"}
+                            initialValue={user.lastname}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please, enter your lastname',
+
+                                },
+                                {
+                                    type: "text",
+                                    message: 'lastname is not valid',
+                                    warningOnly: true,
+
+                                },
+                            ]}
+                        >
+                            <Input placeholder='lastname' style={{ width: '230px', marginRight: '10px' }}
+                                prefix={<UserOutlined />} />
+                        </Form.Item>
+                    </div>
+                    <div style={{ display: 'flex' }}>
+                        <Form.Item
+                            name={"email"}
+                            initialValue={user.email}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please, enter your email',
+
+                                },
+                                {
+                                    type: "email",
+                                    message: 'email is not valid',
+                                    warningOnly: true,
+
+                                },
+                            ]}
+                        >
+                            <Input placeholder='email' style={{ width: '230px', marginRight: '10px' }}
+                                prefix={<MailOutlined />} />
+
+                        </Form.Item>
+                        <Form.Item
+                            name={"password"}
+                            initialValue={user.password}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please, enter your password',
+
+                                },
+                                {
+                                    type: "password",
+                                    message: 'password is not valid',
+                                    warningOnly: true,
+
+                                },
+                            ]}>
+                            <Input.Password placeholder='password' style={{ width: '230px', marginRight: '10px' }}
+                                prefix={<LockOutlined />} />
+                        </Form.Item>
+                    </div>
+                    <div style={{ display: 'flex' }}>
+                        <Form.Item >
+                            <Space.Compact>
+                                <Form.Item
+                                    name={'profil'}
+                                    initialValue={user.profil}
+                                    noStyle
+                                    rules={[{ required: true, message: 'Profil is required' }]}
+                                >
+                                    <Select placeholder="profil" style={{ width: '230px', marginRight: '10px' }}
+                                    >
+                                        <Option value="User">User</Option>
+                                        <Option value="Admin">Admin</Option>
+                                    </Select>
+                                </Form.Item>
+                            </Space.Compact>
+                        </Form.Item>
+                        <Form.Item
+                            name={"phone"}
+                            initialValue={user.phone}
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please, enter your phone number',
+
+                                },
+                                {
+                                    type: "text",
+                                    message: 'phone number is not valid',
+                                    warningOnly: true,
+
+                                },
+                            ]}>
+                            <Input placeholder='phone' style={{ width: '230px', marginRight: '10px' }}
+                                prefix={<PhoneOutlined />} />
+                        </Form.Item>
+                    </div>
+                    <div
+                        style={{ display: 'flex' }}>
+                        <Form.Item >
+                            <Space.Compact>
+                                <Form.Item
+                                    name={'statut'}
+                                    noStyle
+                                    initialValue={user.statut}
+                                    rules={[{ required: false, message: 'Status is required' }]}
+                                >
+                                    <Select placeholder="statut" style={{ width: '230px', marginRight: '10px' }}>
+                                        <Option value="Actif">Actif</Option>
+                                        <Option value="Inactif">Inactif</Option>
+                                    </Select>
+                                </Form.Item>
+                            </Space.Compact>
+                        </Form.Item>
+                        <Form.Item>
+                            <Input type='file' accept='.png,.jpeg,.jpg'
+                                onChange={handleImageSelected}
+                            />
+                        </Form.Item>
+                    </div>
+
+                    <Form.Item>
+                        <Button htmlType='submit' type="primary" style={{ width: '150px' }} onClick={onUpdate}>Update</Button>
+                    </Form.Item>
+                </Card>
             </div>
         </div>
     );
--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Table, Space } from 'antd';
 import axios from 'axios';
 import AddUser from '../../components/user/AddUser';
 import UserDetails from '../../components/user/UserDetails';
 import UpdateUser from '../../components/user/UpdateUser';
+import { SearchOutlined } from '@ant-design/icons';
+import React, { useRef, useState } from 'react';
+import Highlighter from 'react-highlight-words';
+import { Button, Input, Space, Table } from 'antd';
 
 const User = () => {
+
     const url = 'https://test-back.authentify.upowa.org/api/user/all?page=2&size=10000'
     const history = useNavigate();
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
-
-
 
     useEffect(() => {
         let email = sessionStorage.getItem('email');
@@ -23,26 +24,154 @@ const User = () => {
         getUsers();
     }, []);
 
+    const getUsers = () => {
+        setLoading(true)
+        axios.get(url)
+            .then((resp) => {
+                setData(resp.data.content)
+                setLoading(false)
+
+            }
+            )
+            .catch(err => console.log(err))
+    };
+    const onUserAdded = () => {
+        getUsers()
+    };
+
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText('');
+    };
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div
+                style={{
+                    padding: 8,
+                }}
+                onKeyDown={(e) => e.stopPropagation()}
+            >
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{
+                        marginBottom: 8,
+                        display: 'block',
+                    }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({
+                                closeDropdown: false,
+                            });
+                            setSearchText(selectedKeys[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{
+                    color: filtered ? '#1677ff' : undefined,
+                }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: '#ffc069',
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    });
     const columns = [
         {
             title: 'Firstname',
             dataIndex: 'firstname',
-            key: 'firstname'
+            key: 'firstname',
+            width: '30%',
+            ...getColumnSearchProps('firstname'),
         },
         {
             title: 'Lastname',
             dataIndex: 'lastname',
-            key: 'lastname'
+            key: 'lastname',
+            width: '20%',
+            ...getColumnSearchProps('lastname'),
         },
         {
             title: 'Email',
             dataIndex: 'email',
-            key: 'email'
+            key: 'email',
+            width: '20%',
+            ...getColumnSearchProps('email'),
         },
         {
             title: 'Profil',
             dataIndex: 'profil',
-            key: 'profil'
+            key: 'profil',
+            ...getColumnSearchProps('profil'),
         },
         {
             title: 'Action',
@@ -54,22 +183,6 @@ const User = () => {
             ),
         },
     ];
-
-    const getUsers = () => {
-        setLoading(true)
-        axios.get(url)
-            .then((resp) => {
-                // console.log(resp.data)
-                setData(resp.data.content)
-                setLoading(false)
-
-            }
-            )
-            .catch(err => console.log(err))
-    };
-    const onUserAdded = () => {
-        getUsers()
-    };
     return (
         <div style={{ marginTop: '-10px' }}>
 
